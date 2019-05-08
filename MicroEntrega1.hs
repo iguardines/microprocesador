94abc17606bb2c1b3f923bb73897de2dd88db5f2
Ignacio <iguardines@gmail.com>
2019-05-07 22:58:22 -0300

    renombro archivo, cargo los test del punto 4

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -1,12 +1,16 @@
+module MicroEntrega1 where
+
 data Microprocesador = Microprocesador {
 	acumuladorA:: Float,
 	acumuladorB:: Float,
 	pc:: Int,
-	etiqueta::[String],
+	etiqueta::String,
 	memoria::[Int]
 } deriving (Show)
 
-xt8088 = Microprocesador 0 0 0 [] [1..20] 
+xt8088 = Microprocesador 0 0 0 "" (take 1024 (repeat 0)) 
+at8086 = Microprocesador 0 0 0 "" [1..20]
+fp20 = Microprocesador 0 0 0 "" (take 32 (repeat 0))
 
 nop = incpc
 
@@ -20,7 +24,7 @@ add micro = incpc micro {
 }
 
 divide micro
-           | acumuladorB micro == 0 = micro { etiqueta = ["DIVISION BY ZERO"], pc=6 }
+           | acumuladorB micro == 0 = micro { etiqueta = "DIVISION BY ZERO", pc=6 }
            | otherwise = micro { acumuladorA = (acumuladorA micro / acumuladorB micro), acumuladorB = 0}
 
 swap micro = incpc micro {
@@ -40,3 +44,6 @@ str pos val micro = micro {
 
 agregarPosicion  0 valor lista = [valor] ++ drop 1 lista   
 agregarPosicion posicion valor lista = take (posicion - 1) lista ++ [valor]  ++ drop posicion lista
+
+
+mensajeError  = etiqueta
--- microprocesador-test.hs
+++ microprocesador-test.hs
@@ -29,3 +29,50 @@ Microprocesador {acumuladorA = 1.0, acumuladorB = 0.0, pc = 3, etiqueta = [], me
 Microprocesador {acumuladorA = 0.0, acumuladorB = 0.0, pc = 0, etiqueta = [], memoria = [1,5,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
 *Main> str 5 33 xt8088
 Microprocesador {acumuladorA = 0.0, acumuladorB = 0.0, pc = 0, etiqueta = [], memoria = [1,2,3,4,33,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
+
+
+Punto 4)
+
+*MicroEntrega1> (nop . nop . nop) xt8088
+Microprocesador {acumuladorA = 0.0, acumuladorB = 0.0, pc = 3, etiqueta = "", memoria = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+
+
+--4.2 Punto 3
+--LODV 5 tiene 
+--como precondiciones: el acumulador A y B están en cero
+--como post-condiciones: el acumulador A tiene valor 5 y el B cero.
+
+*MicroEntrega1> xt8088
+Microprocesador {acumuladorA = 0.0, acumuladorB = 0.0, pc = 0, etiqueta = "", memoria = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+*MicroEntrega1> lodv 5 xt8088
+Microprocesador {acumuladorA = 5.0, acumuladorB = 0.0, pc = 1, etiqueta = "", memoria = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+
+
+-- Dado un procesador fp20 que tiene acumulador A con 7 y acumulador B con 24, al ejecutar SWAP el acumulador A debe quedar con 24 y el B con 7.
+--precondicion
+*MicroEntrega1> fp20
+Microprocesador {acumuladorA = 0.0, acumuladorB = 0.0, pc = 0, etiqueta = "", memoria = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+
+--postcondicion antes del swap
+*MicroEntrega1> (lodv 7 .  swap .  lodv 24) fp20
+Microprocesador {acumuladorA = 7.0, acumuladorB = 24.0, pc = 3, etiqueta = "", memoria = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+
+--postcondicion despues del swap
+*MicroEntrega1> (swap . lodv 7 .  swap .  lodv 24) fp20
+Microprocesador {acumuladorA = 24.0, acumuladorB = 7.0, pc = 4, etiqueta = "", memoria = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+
+--Luego de ejecutar el programa que suma 10 + 22, el acumulador A debe quedar en 32 y el B en 0.
+*MicroEntrega1> (add . lodv 10 .  swap .  lodv 22) fp20
+Microprocesador {acumuladorA = 32.0, acumuladorB = 0.0, pc = 4, etiqueta = "", memoria = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+
+
+-- 4.3 
+-- Dado el procesador at8086 que tiene los acumuladores en cero, el program counter en 0, sin mensaje de error y una memoria con los siguientes datos: [1..20], le ejecutamos la instrucción STR 2 5. Entonces el procesador at8086 debe quedar con un 5 en la posición 2: [1, 5, 3, 4, 5,... ]
+
+*MicroEntrega1> at8086
+Microprocesador {acumuladorA = 0.0, acumuladorB = 0.0, pc = 0, etiqueta = "", memoria = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
+*MicroEntrega1> str 2 5 at8086
+Microprocesador {acumuladorA = 0.0, acumuladorB = 0.0, pc = 0, etiqueta = "", memoria = [1,5,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
+
+
+
